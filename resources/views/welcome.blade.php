0bfcd8f488ff9bff07648f022c653898fc06abe6
acontrerasc2014 <acontrerasc2014@alu.uct.cl>
2017-08-22 14:30:10 -0300

    practicando vista rutas y rutas controladores y modelos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5</div>mivista
+                <a href="{{ url('mivista') }}"></a>
             </div>
         </div>
     </body>
