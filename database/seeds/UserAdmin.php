ba36d3e4a89649c58d87ee91284471c3d732ad19
root <daniel@holajarvis.com>
2017-09-04 16:02:37 -0300

    seed 1er user

--- database/seeds/UserAdmin.php
+++ database/seeds/UserAdmin.php
@@ -6,7 +6,7 @@ class UserAdmin extends Seeder {
     public function run() {
         DB::table('users')->insert([
             'name' => 'admin',
-            'email' => 'admin',
+            'email' => 'admin@admin.com',
             'password' => bcrypt('admin'),
         ]);
     }
