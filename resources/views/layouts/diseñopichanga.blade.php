7179bcf5301c22a1fffbc2c268e27731d693f07b
Daniel Heresmann <daniel@holajarvis.com>
2017-09-15 00:32:49 -0300

    plantilla administradora

--- resources/views/layouts/diseñopichanga.blade.php
+++ resources/views/layouts/diseñopichanga.blade.php
@@ -2,12 +2,8 @@
 <html lang="es">
 <head>
 	<meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
 	<title>PichangaForAll</title>
-
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
@@ -17,15 +13,12 @@
     {!! Html::style('admin/assets/css/font-awesome.min.css'); !!}
     {!! Html::style('admin/assets/css/font1.css'); !!}
     {!! Html::style('admin/assets/css/themify-icons.css'); !!}
-
 </head>
 <body>
     <div class="wrapper">
         @yield('sidebar')
-
         <div class="main-panel">
             @yield('navbar')
-
             <div class="content">
                 <div class="container-fluid">
                     @yield('contenido')
@@ -34,10 +27,7 @@
         </div>
     </div>
 </body>
-    <!--   Core JS Files   -->
     {!! Html::script('assets/js/jquery-3.2.1.js'); !!}
-    {!! Html::script('assetsj/js/bootstrap.min.js'); !!}
-
     {!! Html::script('admin/assets/js/bootstrap.min.js'); !!}
     {!! Html::script('admin/assets/js/chartist.min.js'); !!}
     {!! Html::script('admin/assets/js/bootstrap-notify.js'); !!}
--- resources/views/userjugador/sidebar.blade.php
+++ resources/views/userjugador/sidebar.blade.php
@@ -4,52 +4,22 @@
             <div class="logo">
                 <a href="#" class="simple-text">
                     PichangaForAll
-                    <i class="fa fa-futbol-o"></i>
+                    <i class="fa fa-futbol-o" aria-hidden="true"></i>
                 </a>
             </div>
 
             <ul class="nav">
 
-                <li class="active">
+                <li id="per" class="active">
                     <a href="user.html">
                         <i class="ti-user"></i>
-                        <p>User Profile</p>
+                        <p>Perfil</p>
                     </a>
                 </li>
                 <li>
-                    <a href="table.html">
-                        <i class="ti-view-list-alt"></i>
-                        <p>Table List</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="typography.html">
-                        <i class="ti-text"></i>
-                        <p>Typography</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="icons.html">
-                        <i class="ti-pencil-alt2"></i>
-                        <p>Icons</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="maps.html">
+                    <a id="map" href="#">
                         <i class="ti-map"></i>
-                        <p>Maps</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="notifications.html">
-                        <i class="ti-bell"></i>
-                        <p>Notifications</p>
-                    </a>
-                </li>
-				<li class="active-pro">
-                    <a href="upgrade.html">
-                        <i class="ti-export"></i>
-                        <p>Upgrade to PRO</p>
+                        <p>Jugadores Cercanos</p>
                     </a>
                 </li>
             </ul>
