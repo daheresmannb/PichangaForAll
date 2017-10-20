f258eeebb10a844b880007fa6f1b019cef87f5c7
Daniel Heresmann <daniel@holajarvis.com>
2017-10-20 18:16:27 -0300

    modelos tablas rol y rel_user_rol

---
+++ app/Models/Rol.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Validator;
+use Illuminate\Database\Eloquent\Model;
+
+class Rol extends Model {
+	protected $table = 'roles'; // NOMBRE DE LA TABLA!!!!!!!
+    protected $fillable = [
+        'id',
+        'nombre',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function RelUserRol() {
+        return $this->hasOne('App\Models\RelUserRol');
+    }
+
+    public function Validar($array){
+    	$validator = Validator::make(
+    		$array, [
+    			'nombre' => 'required',
+			]
+		);
+		return $validator;
+    }
+}
