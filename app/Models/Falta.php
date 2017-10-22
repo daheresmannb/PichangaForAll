477aae66dfb249f1965b64389914b9db4caf98ec
Daniel Heresmann <daniel@holajarvis.com>
2017-10-22 15:57:11 -0300

    modelo tabla faltas

---
+++ app/Models/Falta.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Validator;
+use Illuminate\Database\Eloquent\Model;
+
+class Falta extends Model {
+	protected $table = 'roles'; // NOMBRE DE LA TABLA!!!!!!!
+    protected $fillable = [
+        'id',
+        'jugador_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function Validar($array){
+    	$validator = Validator::make(
+    		$array, [
+    			'jugador_id' => 'required',
+			]
+		);
+		return $validator;
+    }
+}
