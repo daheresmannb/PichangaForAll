ff103572c1446181257c104c078aa830b5be8fce
Tintolio <ticast.13@gmail.com>
2017-11-01 17:50:50 -0300

    mejora vista responsive crear partidos

--- resources/views/userjugador/crearpartido.blade.php
+++ resources/views/userjugador/crearpartido.blade.php
@@ -7,40 +7,44 @@
 {!!Html::script('assets/js/jquery.datetimepicker.full.js'); !!}
 
 <div class="container-fluid">
-    <div class="col-md-4">
-                 <div class="row">
-                            <h1>crea tu partido </h1>
-                    </div>
-
-                <form role="form-gruop">
-
-                                <div class="form-horizontal">
-                                <label>seleccione recinto</label>
-                                         <select class="custom-select">
+      <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 text">
+                            <h3><strong>Crea tu Partido</strong> </h3>
+             
+                        </div>
+      </div>
+ <div class="row">
+      <div class="col-sm-6 col-sm-offset-3 form-box">                         
+          <div class="form-group row">
+                               <div class="form-horizontal row">
+                                <label class="col-md-6 col">seleccione recinto</label>
+                                         <select class="custom-select col-md-6 col">
                                                <option selected>recinto</option>
                                              <option value="1">quilas</option>
                                          </select>
                                 </div>
-                                <div>
-                                    <label>fecha y hora de inicio</label>
-                                   
-                                   <input id="partidotime">
-                                   <script>
+                                <span class="help-block"></span> 
+                                <div class="form-horizontal row" >
+                                  <label class="col-md-6 col">fecha y hora de inicio</label>
+                                  <input id="partidotime" class="col-md-6 col">
+                                  <script>
                                        $("#partidotime").datetimepicker({
                                         autoclose: true
                                        });
-                                   </script>
-                                   <br>
-                                <label>fecha y hora de termino</label>
-                                   
-                                   <input id="partidotime2">
-                                   <script>
+                                  </script>
+                                </div>
+                                <span class="help-block"></span>
+                                <div class="form-horizontal row">
+                                <label class="col-md-6 col">fecha y hora de termino</label>
+                                  <input id="partidotime2" class="col-md-6 col">
+                                  <script>
                                        $("#partidotime2").datetimepicker({
                                         autoclose: true
                                        });
                                    </script>
                                 </div>
-                                <div class="form-horizontal">
+                                <span class="help-block"></span>   
+                                <div class="col-md-6 col-md-offset-3 ">
                                 {!! Form::submit(
                                             'crear partido',
                                             array(
@@ -48,6 +52,6 @@
                                 !!}
                                 </div>
 
-                </form>
+                </div>
     </div>
 </div>
--- resources/views/userjugador/infouser.blade.php
+++ resources/views/userjugador/infouser.blade.php
@@ -6,7 +6,7 @@
 <div class="container-fluid" >
 	<div class="row">
                         <div class="col-sm-8 col-sm-offset-2 text">
-                            <h1><strong>Informacion de usuario</strong> </h1>
+                            <h3><strong>Informacion de usuario</strong> </h3>
              
                         </div>
   </div>
